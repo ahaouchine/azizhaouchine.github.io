5b732e60727ecbad15a1d3957cf2bd7c6b397cb9
ahaouchine <haouchineaziz3@gmail.com>
2025-02-26 12:51:54 -0800

    first commit

---
+++ app/components/education.tsx
@@ -0,0 +1,19 @@
+import react from "react"
+
+export default function Educations() {
+    return (
+        <div className="p-1">
+            <h5 className="mb-2 text-m font--bold traking-tight"></h5>
+            <div className="mb-3 font-normal">
+                <strong>California State University, Domingueez Hills</strong>
+                <p className="text-sm font-light"> Bachelor of Science, XXX</p>
+                <p className="text-sm font-light italic">January 20XX - may 20XX</p>
+                <hr></hr>
+                <p className="text-sm font-light font"> 
+                    At California State University, Dominguez Hills, I am pursing a Bachelor of Science degree...
+                 </p> 
+            </div>  
+
+        </div>
+    )
+}
